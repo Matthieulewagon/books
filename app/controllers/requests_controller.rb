cfeae753fad68d7a88e9b314a7fa7bc02b58956a
Matthieu Lombard <matthieu.lombard@hotmail.be>
2017-01-09 21:41:14 +0100

    permission user sign up

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,4 +1,6 @@
 class RequestsController < ApplicationController
+  before_action :if_not_sign_up, only: [:create]
+
   def new
     @book = Book.find(params[:book_id])
     @user = User.find(@book.user_id)
@@ -25,5 +27,11 @@ class RequestsController < ApplicationController
 
   def book_params
     params.require(:book).permit(:user_id, :description, :title,  :price, :picture, :campus, :year, :picture_cache)
+  end
+
+  def if_not_sign_up
+    if !user_signed_in?
+      redirect_to new_user_registration_path
+    end
   end
 end
