82b9cd8f77c8b5be746d81b5b83420c86fabf7f8
Matthieu Lombard <matthieu.lombard@hotmail.be>
2017-01-27 18:40:05 +0100

    show profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
+    @books = Book.where(user_id: current_user.id)
+    @user = User.find(current_user.id)
   end
 
   def edit
